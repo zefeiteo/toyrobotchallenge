3353ff14141dd227d5f40434e1fdf3a97ff0c476
Ze Fei Teo <66097256+zefeiteo@users.noreply.github.com>
2021-11-12 00:06:37 +1100

    Configure pyproject and metadata

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+requirements = [
+    "argparse>=1.4.0",
+]
+
+setup(
+    author="Ze Fei Teo",
+    author_email="zefeiteo@gmail.com",
+    python_requires=">=3.8.10",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Toy robot that traverses on a finite surface",
+    entry_points={"console_scripts": ["toy_robot=prog:main"]},
+    install_requires=requirements,
+    license="MIT license",
+    keywords="toyrobot",
+    name="toyrobot",
+    packages=find_packages(include=["toyrobot"]),
+    test_suite="tests",
+    url="https://github.com/zefeiteo/toyrobotchallenge",
+    version="0.1.0",
+    zip_safe=False,
+)
